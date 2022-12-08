71581c17d3a2b87fcf7113da241955820699524a
Maria Daan <44019945+mariadaan@users.noreply.github.com>
2022-12-08 14:58:57 +0100

    hele boel omgegooid naar idee van nicky bedankt @diemen112

--- Day_07/day7.cpp
+++ Day_07/day7.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <map>
 #include <string>
-#include <string_view>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
-class Directory
+template <typename T>
+void print(const T toPrint)
 {
-public:
-	int myFilesSize;
-	std::vector<Directory> myDirectories;
-};
- 
-void printMap(std::string_view comment, const std::map<std::string, int>& map)
-{
-	std::cout << comment;
-
- for (std::map<std::string, int>::const_iterator it = map.begin(); it != map.end(); it++)
-	 std::cout << it->first << " = " << it->second << "; ";
-	std::cout << '\n';
+	std::cout << toPrint << std::endl;
 }
 
-std::vector<std::string> splitStr(std::string str, std::string delimiter)
+void printStrVector(const std::vector<std::string> strlist)
+{
+	for (std::vector<std::string>::const_iterator it = strlist.begin(); it != strlist.end(); it++)
+		std::cout << *it << ", ";
+	std::cout << std::endl;
+}
+
+void printMap(const std::map<std::string, int>& map)
+{
+	for (std::map<std::string, int>::const_iterator it = map.begin(); it != map.end(); it++)
+		std::cout << it->first << " = " << it->second << "; ";
+	std::cout << std::endl;
+}
+
+std::vector<std::string> getInput(const std::string filename)
+{
+	std::ifstream inputFile(filename);
+	std::vector<std::string> allLines;
+	std::string line;
+
+	while (std::getline(inputFile, line))
+		allLines.push_back(line);
+	inputFile.close();
+	return allLines;
+}
+
+std::vector<std::string> splitStr(std::string str, const std::string delimiter)
 {
 	std::vector<std::string> splitted;
 	size_t pos = 0;
@@ -36,192 +54,75 @@ std::vector<std::string> splitStr(std::string str, std::string delimiter)
 	return splitted;
 }
 
-std::string getDirName(std::string line)
+std::map<std::string, int> getDirSizes(const std::vector<std::string> &terminalInput)
 {
-	return splitStr(line, " ")[2];
-}
-
-int getFileSize(std::string line)
-{
-	return stoi(splitStr(line, " ")[0]);
-}
-
-int getDirSize(std::string line, std::string currentDir, std::map<std::string, int> &dirSizes)
-{
-	int dirSize;
-	std::string dirToAdd = splitStr(line, " ")[1];
-	return dirSizes[dirToAdd];
-}
-
-std::map<std::string, int> saveFiles(std::vector<std::string> terminalInput)
-{
-	std::string line;
 	std::map<std::string, int> dirSizes;
-	std::string currentDir = "/";
-	int fileSize = 0;
+	std::vector<std::string> dirList;
+	std::ostringstream fullPath;
+	int size = 0;
 
-
-	for (int i = 0; i < terminalInput.size(); i++)
+	for (std::vector<std::string>::const_iterator itLine = terminalInput.begin(); itLine < terminalInput.end(); itLine++)
 	{
-		line = terminalInput[i];
-		if (line == "$ cd ..")
-			continue;
-		if (line.compare(0,5,"$ cd ") == 0)
-			currentDir = getDirName(line);
-		else if (isdigit(line[0]))
+		std::vector<std::string> splitLine = splitStr(*itLine, " ");
+		if (*itLine == "$ cd ..")
 		{
-			fileSize = getFileSize(line);
-			dirSizes[currentDir] += fileSize;
+			dirList.pop_back();
+		}
+		else if ((*itLine).substr(0, 5) == "$ cd ")
+		{
+			dirList.push_back(splitLine[2]);
+		}
+		else if (isdigit((*itLine)[0]))
+		{
+			size = stoi(splitLine[0]);
+			for (std::vector<std::string>::iterator itDirs = dirList.begin(); itDirs < dirList.end(); itDirs++)
+			{
+				// works like a join function, found this on stackoverflow lol
+				std::copy(dirList.begin(), itDirs + 1, std::ostream_iterator<std::string>(fullPath, "-"));
+				dirSizes[fullPath.str()] += size; // save path in dict and add size of files in current folder
+				fullPath.str(std::string()); // clear fullpath
+			}
 		}
 	}
-	printMap("my map: ", dirSizes);
 	return dirSizes;
 }
 
-// void dirInDir(std::vector<std::string> terminalInput, std::map<std::string, int> &dirSizes)
-// {
-// 	std::string line;
-// 	std::string currentDir = "/";
-// 	int dirSize = 0;
-
-// 	for (int i = 0; i < terminalInput.size(); i++)
-// 	{
-// 		line = terminalInput[i];
-// 		if (line == "$ cd ..")
-// 			continue;
-// 		if (line.compare(0,5,"$ cd ") == 0)
-// 			currentDir = getDirName(line);
-// 		else if (line.compare(0,4,"dir ") == 0)
-// 		{
-// 			dirSize = getDirSize(line, currentDir, dirSizes);
-// 			dirSizes[currentDir] += dirSize;
-// 		}
-// 	}
-// 	printMap("my map: ", dirSizes);
-// }
-
-
-// void dirInDir(std::string line, std::string currentDir, std::map<std::string, int> &dirSizes)
-// {
-// 	std::string currentDir = "/";
-// 	int dirSize = 0;
-
-// 	if (line == "$ cd ..")
-// 		continue;
-// 	if (line.compare(0,5,"$ cd ") == 0)
-// 		currentDir = getDirName(line);
-// 	else if (line.compare(0,4,"dir ") == 0)
-// 	{
-// 		dirSize = getDirSize(line, currentDir, dirSizes);
-// 		dirSizes[currentDir] += dirSize;
-// 	}
-// }
-
-int getMaxDepth(std::vector<std::string> terminalInput)
+int findSmallDirs(const std::map<std::string, int> dirSizes)
 {
-	std::string line;
-	std::string currentDir = "/";
-	int depth = 0;
-	int maxDepth = 0;
-
-	for (int i = 0; i < terminalInput.size(); i++)
+	int total = 0;
+	for (std::map<std::string, int>::const_iterator it = dirSizes.begin(); it != dirSizes.end(); it++)
 	{
-		line = terminalInput[i];
-		if (line != "$ cd .." &&  line.compare(0,5,"$ cd ") == 0)
-		{
-			currentDir = getDirName(line);
-			depth++;
-			maxDepth = depth > maxDepth ? depth : maxDepth;
-		}
-		else if (line == "$ cd ..")
-			depth--;
+		if (it->second < 100000)
+			total += it->second;
 	}
-	std::cout << "maxDepth: " << maxDepth << std::endl;
-	return maxDepth;
+	return total;
 }
 
-/* 
-go most deep
-save maxDepth
-dir in dir toepassen depthlevel van onder naar boven
- */
-
-std::vector<std::string> getInput(std::string filename)
+int findDirToDel(std::map<std::string, int> dirSizes)
 {
-	std::ifstream inputFile(filename);
-	std::vector<std::string> allLines;
-	std::string line;
-
-	while (std::getline(inputFile, line))
+	int usedSize = dirSizes["/-"];
+	int freeSize = 70000000 - usedSize;
+	int requiredDel = 30000000 - freeSize;
+	int smallestDirToDel = usedSize;
+	for (std::map<std::string, int>::iterator it = dirSizes.begin(); it != dirSizes.end(); it++)
 	{
-		allLines.push_back(line);
+		if(it->second > requiredDel && it->second < smallestDirToDel)
+			smallestDirToDel = it->second;
 	}
-	inputFile.close();
-	return allLines;
+	return smallestDirToDel;
 }
-
-void fillDirs(std::vector<std::string> terminalInput, std::map<std::string, int> &dirSizes)
-{
-	std::string line;
-	std::string currentDir = "/";
-	int depth = 0;
-	int maxDepth = 0;
-
-	for (int i = 0; i < terminalInput.size(); i++)
-	{
-		line = terminalInput[i];
-		if (line != "$ cd .." &&  line.compare(0,5,"$ cd ") == 0)
-		{
-			currentDir = getDirName(line);
-			depth++;
-			// if (depth == maxDepth - 1)
-			// {
-			// 	dirInDir()
-			// }
-		}
-		else if (line == "$ cd ..")
-			depth--;
-	}
-}
-
 
 int main()
 {
-	std::string filename = "testData.txt";
+	std::string filename = "realData.txt";
 	std::vector<std::string> terminalInput = getInput(filename);
-	std::map<std::string, int> dirSizes;
+	std::map<std::string, int> dirSizes = getDirSizes(terminalInput);
+	// printMap(dirSizes);
+	int answerPartOne = findSmallDirs(dirSizes);
+	print(answerPartOne);
 
-	dirSizes = saveFiles(terminalInput);
-	fillDirs(terminalInput, dirSizes);
-	// dirInDir(filename, dirSizes);
-	getMaxDepth(terminalInput);
+	int answerPartTwo = findDirToDel(dirSizes);
+	print(answerPartTwo);
+
+	return (0);
 }
-
-// 	// Create a map of three (string, int) pairs
-// 	std::map<std::string, int> map;
-
-// 	map["huh"] = 8;
-
-// 	map.insert ( std::pair<std::string,int>("hallo",100) );
-// 	map.insert ( std::pair<std::string,int>("jaa",200) );
-
-	
-// 	// map = {{"CPU", 10}, {"GPU", 15}, {"RAM", 20}};
- 
-// 	printMap("1) Initial map: ", map);
- 
-// 	map["CPU"] = 25; // update an existing value
-// 	map["SSD"] = 30; // insert a new value
-// 	printMap("2) Updated map: ", map);
- 
-// 	// using operator[] with non-existent key always performs an insert
-// 	std::cout << "3) map[UPS] = " << map["UPS"] << '\n';
-// 	printMap("4) Updated map: ", map);
- 
-// 	map.erase("GPU");
-// 	printMap("5) After erase: ", map);
- 
- 
-// 	map.clear();
-// 	std::cout << std::boolalpha << "8) Map is empty: " << map.empty() << '\n';
-// }
