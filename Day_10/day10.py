1868e6edbae5d078743aeabe0a8b1513701bcdc5
Maria Daan <mariadaan@gmail.com>
2023-06-08 12:14:54 +0200

    cleanup var names and structure

--- Day_10/day10.py
+++ Day_10/day10.py
@@ -1,6 +1,7 @@
-instructions = []
+""" https://adventofcode.com/2022/day/10 """
 
 # parse data
+all_signals = []
 with open("realdata.txt", "r") as file:
 	for line in file:
 		parts = line.strip().split(" ")
@@ -12,7 +13,7 @@ with open("realdata.txt", "r") as file:
 			x_change = 0
 			duration = 1
 		entry = {"instruction": instruction, "x_change": x_change, "duration": duration}
-		instructions.append(entry)
+		all_signals.append(entry)
 
 # initialize variables
 x = 1
@@ -21,9 +22,9 @@ important_cycles = [20, 60, 100, 140, 180, 220]
 interesting_signals = []
 
 # save all interesting signals
-for operation in instructions:
-	cycle += operation["duration"]
-	x += operation["x_change"]
+for signal in all_signals:
+	cycle += signal["duration"]
+	x += signal["x_change"]
 	if cycle in important_cycles or cycle + 1 in important_cycles:
 		interesting_signals = [signal for signal in interesting_signals if cycle - 1 not in signal]
 		interesting_signals.append((cycle, x))
@@ -36,26 +37,23 @@ for signal in interesting_signals:
 	if (cycle % 2 != 0):
 		cycle += 1
 	signal_strength_sum += cycle * x
-
 print("answer part 1: ", signal_strength_sum)
 
-x = 1
+# draw all pixels
+print("\nanswer part 2:", end='')
+sprite_center = 1
 pixel = 0
-# loop through all pixels
-for operation in instructions:
+for signal in all_signals:
 	if (pixel % 40 == 0):
 		print()
-	# define position of sprite
-	sprite = x - 1, x, x + 1
+	sprite = sprite_center - 1, sprite_center, sprite_center + 1
 	to_print = '#' if pixel % 40 in sprite else '.'
 	print(to_print, end='')
-	if (operation["instruction"] == "addx"):
+	if (signal["instruction"] == "addx"):
 		sub_pixel = pixel + 1
 		if (sub_pixel % 40 == 0):
 			print()
 		to_print = '#' if sub_pixel % 40 in sprite else '.'
 		print(to_print, end='')
-
-	# update values
-	pixel += operation["duration"]
-	x += operation["x_change"]
+	pixel += signal["duration"]
+	sprite_center += signal["x_change"]
